877e607fd2cdf42ef66ed1865824d90794f8e262
alejanr2286 <42847884+alejanr2286@users.noreply.github.com>
2018-11-15 12:43:20 -0800

    Update card.cpp

--- card.cpp
+++ card.cpp
@@ -7,7 +7,23 @@ description:
 */
 
 #include "card.h"
+#include "deck.h"
 
 #include <iostream>
 #include <string>
 #include <vector>
+
+Card::Card(char _s)
+{
+  symbol = _s;
+}
+
+Card::Card ( int _v) 
+{
+
+}
+
+void Card::add_decl ( Deck _d)
+{
+
+}
